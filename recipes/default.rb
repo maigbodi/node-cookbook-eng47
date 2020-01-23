e878863eeabd07b149a0c9346faff4a78f319448
maigbodi <34092907+maigbodi@users.noreply.github.com>
2020-01-23 04:42:29 +0000

    binary

--- recipes/default.rb
+++ recipes/default.rb
@@ -30,6 +30,11 @@ link '/etc/nginx/sites-enabled/default' do
   action :delete
 end
 
-include_recipe "nodejs"
+node.default['nodejs']['version'] = '12.14.1'
+node.default['nodejs']['binary']['url'] = 'https://nodejs.org/dist/v12.14.1/node-v12.14.1.tar.xz'
+node.default['nodejs']['binary']['checksum'] = '6cd28a5e6340f596aec8dbfd6720f444f011e6b9018622290a60dbd17f9baff6'
+ 
+include_recipe 'nodejs::nodejs_from_binary'
+
 
 nodejs_npm 'pm2'
