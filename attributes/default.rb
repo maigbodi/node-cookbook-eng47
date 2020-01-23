413b37b9bc5b30c7e87f2e9f231f0014b622429a
maigbodi <34092907+maigbodi@users.noreply.github.com>
2020-01-23 04:49:05 +0000

    version 12

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
-default['nodejs']['repo']         = 'https://deb.nodesource.com/node_12.x'
-default['nodejs']['version'] = '12.14.1'
+node.default['nodejs']['version'] = '12.14.1'
+node.default['nodejs']['binary']['url'] = 'https://nodejs.org/dist/v12.14.1/node-v12.14.1.tar.xz'
+node.default['nodejs']['binary']['checksum'] = '6cd28a5e6340f596aec8dbfd6720f444f011e6b9018622290a60dbd17f9baff6'
 default['nginx']['proxy_port'] = 3000
